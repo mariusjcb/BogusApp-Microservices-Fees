91cc2f18df3a52b806275abcfb261e195d9a027d
Marius Ilie <marius@iliemarius.ro>
2021-01-19 15:58:48 +0200

    Docker fix

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,9 +1,10 @@
-import App
+import Fees
 import Vapor
 
 var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
+app.http.server.configuration.port = Int(Environment.get("PORT") ?? "8083" ) ?? 8083
 defer { app.shutdown() }
 try configure(app)
 try app.run()
